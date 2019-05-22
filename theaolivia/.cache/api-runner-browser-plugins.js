d97557de23d66c09b5c38bc54e8b15e039a6e189
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-05-22 19:51:01 +0700

    adding modules

---
+++ theaolivia/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,19 @@
+module.exports = [{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/node_modules/gatsby-plugin-transition-link/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"google":{"families":["Droid Sans","Droid Serif"]}},
+    },{
+      plugin: require('/Users/theaolivia/projects/teaolivia.github.io/theaolivia/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
