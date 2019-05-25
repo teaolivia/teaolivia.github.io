0f4214729492afe8e5db2ba69c4358dc2487a0af
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-05-25 16:31:17 +0700

    adding blog features started

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
