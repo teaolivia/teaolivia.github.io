78f0943ee418f212cdefe421de374c109c215f8b
Thea Yusuf <thea.yusuf@klarna.com>
2022-03-09 09:12:59 +0100

    Updates

--- component---src-pages-404-js-d14df127baf169fb18d3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),u=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"Whoops :("),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-d14df127baf169fb18d3.js.map
