5fb55df21f5adb70abaf4c30ebfb60eff61c51b5
Thea Yusuf <thea.yusuf@klarna.com>
2022-03-09 09:05:43 +0100

    Updates

---
+++ component---src-pages-resume-js-71cf29ff2323858110f3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktheapisan=self.webpackChunktheapisan||[]).push([[903],{5248:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var u=t(7294);t.p;var a=function(){return u.createElement("p",null,"lol")}}}]);
+//# sourceMappingURL=component---src-pages-resume-js-71cf29ff2323858110f3.js.map
