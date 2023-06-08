183e19b35ba22b7cc54ae6de8a92a4327334dfb3
Thea Yusuf <theaolivia.mail@gmail.com>
2023-06-08 22:16:54 +0200

    reset gatsby

--- src/components/image.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import pp from "../images/pp.png"
-
-function Image() {
-  return <img src={pp} padding="10" width="200" height="200" alt="ProfilePicture" />
-}
-
-export default Image
--- src/pages/resume.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ResumeFile from "../files/resume.pdf"
-
-function Resume() {
-    return (<p>lol</p>)
-  }
-  
-export default Resume
